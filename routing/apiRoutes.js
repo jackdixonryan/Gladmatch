45571ef5d1a77575a6f452d4a5bb51d59a40508c
Jack Ryan <jack.dixon.ryan@gmail.com>
2018-08-16 10:36:45 -0600

    Form is now working and pushing data to the API. Will need to create some dummy data and figure out how to tap a matches file without using fs--maybe just export the JSON?

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -2,17 +2,29 @@ module.exports = app => {
 
     //These two constants here allow our api routes to access the locally stored JSON file and access it before running any gets or posts on it
     const fs= require('fs');
-    const matches = JSON.parse(fs.readFileSync('./app/data/matches.json', 'utf8'));
 
     //Our get request for the API now renders the matches stored in matches.json
     app.get('/api/matches', (req, res) => {
         return res.json(matches);
     });
 
-    //This is a bit more difficult and is likely broken. The idea is that each post request modifies the matches object parsed above and rewrites matches.json with the newly added response. 
+    //This is a bit more difficult and is likely broken. The idea is that each post request modifies the matches object parsed above and rewrites matches.json with the newly added response.
+    let matches = [
+        {
+            "name" : "Spartacus",
+            "answers": [
+                0,
+                1,
+                2,
+                3,
+                4
+            ]
+        }
+    ];
+
     app.post('/api/matches', (req, res) => {
         let newEntry = req.body;
+        matches.push(newEntry);
         res.json(newEntry);
-        console.log(newEntry);
     });
 }
