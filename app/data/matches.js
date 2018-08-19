027f618fd5b8775ef7890eac9bd436f3686c402c
Jack Ryan <jack.dixon.ryan@gmail.com>
2018-08-19 11:07:35 -0600

    Finished product.

--- app/data/matches.js
+++ app/data/matches.js
@@ -2,77 +2,197 @@ module.exports = [
     {
         name: 'Rick Sanchez',
         scores: [
+            '5',
+            '5',
             '1',
-            '2',
-            '1',
-            '4',
+            '5',
+            '5',
+            '3',
             '5'
         ],
-        total : 15
+        total : 29,
+        image: 'https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest/scale-to-width-down/620?cb=20160923150728'
     },
     {
-        name: 'Morty Smith'
-        scores: [],
-        total: 
+        name: 'Morty Smith',
+        scores: [
+            '1',
+            '1',
+            '2',
+            '3',
+            '2',
+            '3',
+            '3' 
+        ],
+        total: 15,
+        image: 'https://vignette.wikia.nocookie.net/rickandmorty/images/4/41/Morty_Smith.jpg/revision/latest?cb=20170217193441'
     },
     {
-        name: 'Beth Smith'
-        scores: [],
-        total:
+        name: 'Beth Smith',
+        scores: [
+            '4',
+            '2',
+            '1',
+            '1',
+            '4',
+            '3',
+            '5'
+        ],
+        total: 20,
+        image: 'https://vignette.wikia.nocookie.net/rickandmorty/images/5/58/Beth_Smith.png/revision/latest?cb=20151204220729'
     },
     {
-        name: 'Jerry Smith'
-        scores: [],
-        total:
+        name: 'Jerry Smith',
+        scores: [
+            '1',
+            '1',
+            '4',
+            '1',
+            '1',
+            '5',
+            '1'
+        ],
+        total: 14, 
+        image: 'https://vignette.wikia.nocookie.net/rickandmorty/images/f/f1/Jerry_Smith.png/revision/latest/scale-to-width-down/620?cb=20160923151111'
     },
     {
-        name: 'Summer Smith'
-        scores: [],
-        total:
+        name: 'Summer Smith',
+        scores: [
+            '4',
+            '1',
+            '1',
+            '5',
+            '3',
+            '2',
+            '1'
+        ],
+        total: 17,
+        image: 'https://vignette.wikia.nocookie.net/rickandmorty/images/a/ad/Summer_is_cool.jpeg/revision/latest/scale-to-width-down/620?cb=20160919183158'
     },
     {
-        name: 'Xenon Bloom'
-        scores: [],
-        total:
+        name: 'Xenon Bloom',
+        scores: [
+            '5',
+            '3',
+            '4',
+            '1',
+            '5',
+            '5',
+            '3'
+        ],
+        total: 26,
+        image: 'https://vignette.wikia.nocookie.net/rickandmorty/images/0/0b/Dr._Xenon_Bloom_HQ/revision/latest/scale-to-width-down/450?cb=20150930231812'
     },
     {
         name: 'Revolio Clockberg, Jr.',
-        scores: [],
-        total:
+        scores: [
+            '3',
+            '5',
+            '5',
+            '1',
+            '4',
+            '5',
+            '1'
+        ],
+        total: 24,
+        image: 'https://vignette.wikia.nocookie.net/rickandmorty/images/5/56/Gearhead.png/revision/latest/scale-to-width-down/620?cb=20140421055739'
     },
     {
         name: 'Bird Person',
-        scores: [],
-        total:
+        scores: [
+            '3',
+            '1',
+            '1',
+            '1',
+            '5',
+            '1',
+            '5'
+        ],
+        total: 17,
+        image: 'https://vignette.wikia.nocookie.net/rickandmorty/images/b/b1/Birdperson.png/revision/latest?cb=20140415133633'
     },
     {
         name: 'Unity',
-        scores: [],
-        total:
+        scores: [
+            '5',
+            '2',
+            '5',
+            '4',
+            '5',
+            '1',
+            '5'
+        ],
+        total: 28,
+        image: 'https://vignette.wikia.nocookie.net/rickandmorty/images/4/45/Unity_Assimilation.png/revision/latest/scale-to-width-down/620?cb=20160923075643'
     },
     {
-        name: 'Mr. Beauregard'
-        scores: [],
-        total:
+        name: 'Mr. Beauregard',
+        scores: [
+            '3',
+            '1',
+            '4',
+            '1',
+            '4',
+            '5',
+            '3'
+        ],
+        total: 21,
+        image: 'https://vignette.wikia.nocookie.net/rickandmorty/images/9/97/MrBeauregard.png/revision/latest/scale-to-width-down/620?cb=20160921081404'
     },
     {
-        name: 'Zeep Xanflorp'
-        scores: [],
-        total:
+        name: 'Zeep Xanflorp',
+        scores: [
+            '5',
+            '5',
+            '1',
+            '4',
+            '5',
+            '2',
+            '5'
+        ],
+        total: 27,
+        image: 'https://vignette.wikia.nocookie.net/rickandmorty/images/1/16/Zeep.png/revision/latest?cb=20150901180927'
     },
     {
-        name: 'Shrimply Pibbles'
-        scores: [],
-        total:
+        name: 'Shrimply Pibbles',
+        scores: [
+            '3',
+            '1',
+            '1',
+            '1',
+            '5',
+            '3',
+            '1'
+        ],
+        total: 15,
+        image: 'https://vignette.wikia.nocookie.net/rickandmorty/images/2/22/S2e8_Shrimply_pibbles_records.png/revision/latest/scale-to-width-down/619?cb=20170813092236'
     },
     {
-        name: 'Jaguar'
-        scores: [],
-        total:
+        name: 'Jaguar',
+        scores: [
+            '2',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '3'
+        ],
+        total: 10,
+        image: 'https://vignette.wikia.nocookie.net/rickandmorty/images/2/2c/Jaguar1.png/revision/latest?cb=20170807135809'
     },
     {
-        name: 'The President'
-        scores: [],
-        total:
+        name: 'The President',
+        scores: [
+            '5',
+            '3',
+            '4',
+            '4',
+            '5',
+            '4',
+            '1'
+        ],
+        total: 26,
+        image: 'https://vignette.wikia.nocookie.net/rickandmorty/images/e/ea/President.png/revision/latest?cb=20150829164829'
     },
-]
+];
